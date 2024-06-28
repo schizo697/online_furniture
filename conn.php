f243216086497bfbd5e1a7c1c211c3293e08bdf9
chanie55 <anya.sc30@gmail.com>
2024-06-27 23:47:50 -0700

    login signup w/ db

---
+++ conn.php
@@ -0,0 +1,13 @@
+<?php 
+$serverename = "localhost";
+$username = "root";
+$password = "";
+$dbname = "mpmdb";
+
+$conn = new mysqli($serverename, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connecton Failed: " . $conn->connect_error);
+
+}
+?>
--- signup.php
+++ signup.php
@@ -7,6 +7,9 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
     <title>MPM: Furniture Shop</title>
+    <style>
+        <?php include 'main/css/login.css'; ?>
+    </style>
 </head>
 <body>
 
@@ -27,17 +30,121 @@
                      <h2>Register</h2>
                      <p>Some text here</p>
                 </div>
-                <div class="input-group mb-3">
-                    <input type="text" class="form-control form-control-lg bg-light fs-6" placeholder="Email address">
-                </div>
-                <div class="input-group mb-1">
-                    <input type="password" class="form-control form-control-lg bg-light fs-6" placeholder="Password">
-                </div>
-                <div class="input-group mb-3">
-                    <button class="btn btn-lg btn-primary w-100 fs-6">Login</button>
-                </div>
-                <div class="row">
-                    <small>Already have an account? <a href="login.php">Login</a></small>
+                <?php
+                    include 'conn.php';
+                    if (isset($_POST['first_name']) && isset($_POST['last_name']) && isset($_POST['gender']) && isset($_POST['contact']) && isset($_POST['address']) && isset($_POST['username']) && isset($_POST['password'])) {
+                        $first_name = $_POST['first_name'];
+                        $last_name = $_POST['last_name'];
+                        $gender = $_POST['gender'];
+                        $contact = $_POST['contact'];
+                        $address = $_POST['address'];
+                        $username = $_POST['username'];
+                        $password = $_POST['password'];
+                        $encrypted = password_hash($password, PASSWORD_DEFAULT);
+
+                        $check_username = "SELECT username FROM useraccount WHERE username = '$username'";
+                        $check_result = mysqli_query($conn, $check_username);
+
+                        if ($check_result && mysqli_num_rows($check_result) > 0) {
+                            // sweetalert exist
+                            echo "<script>
+                            Swal.fire({
+                                text: 'Username Already Exist',
+                                icon: 'warning',
+                                confirmButtonColor: '#3085d6',
+                            });
+                            </script>";
+                        } else {
+                            $sql = "INSERT INTO userinfo (firstname, lastname, gender, contact, address) VALUES ('$first_name', '$last_name', '$gender', '$contact', '$address')";
+
+                            if ($conn->query($sql) === TRUE) {
+                                $info_id = $conn->insert_id;
+                                $sql = "INSERT INTO useraccount (username, password, levelid, infoid, status) VALUES ('$username', '$encrypted', 3, '$info_id', 1)";
+
+                                    if ($conn->query($sql) === TRUE) {
+                                    // sweetalert success
+                                    echo "<script>
+                                        Swal.fire({
+                                            position: 'center',
+                                            icon: 'success',
+                                            title: 'Account Created Successfully',
+                                            showConfirmButton: false
+                                        });
+                                        setTimeout(function() {
+                                            window.location.href = 'login.php';
+                                        }, 2000); // 2 seconds delay
+                                    </script>";
+                                } else {
+                                    // sweetalert error
+                                    echo "<script>
+                                        Swal.fire({
+                                            icon: 'error',
+                                            text: 'Something went wrong!',
+                                        });
+                                    </script>";
+                                    }
+                                }
+                        }
+                    }
+                    ?>
+                <div class="col-md-12">
+                    <div class="row justify-content-center align-items-center">
+                        <form action="" method="POST" class="w-100">
+                            <div class="row mb-3">
+                                <div class="col-md-6">
+                                    <div class="input-group">
+                                        <input type="text" name="first_name" id="first_name" class="form-control form-control-lg bg-light fs-6" placeholder="First Name" required>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="input-group">
+                                        <input type="text" name="last_name" id="last_name" class="form-control form-control-lg bg-light fs-6" placeholder="Last Name" required>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <div class="col-md-6">
+                                    <div class="input-group">
+                                        <input type="text" name="gender" id="gender" class="form-control form-control-lg bg-light fs-6" placeholder="Gender" required>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="input-group">
+                                        <input type="text" name="contact" id="contact" class="form-control form-control-lg bg-light fs-6" placeholder="Contact" required>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-12">
+                                <div class="input-group">
+                                    <input type="text" name="address" id="address" class="form-control form-control-lg bg-light fs-6" placeholder="Address" required>
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <div class="col-md-6">
+                                    <div class="input-group">
+                                        <input type="text" name="username" id="username" class="form-control form-control-lg bg-light fs-6" placeholder="Username" required>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="input-group">
+                                        <input type="password" name="password" id="password" class="form-control form-control-lg bg-light fs-6" placeholder="Password" required>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <div class="col-md-12">
+                                    <div class="input-group">
+                                        <button type="submit" name="submit" class="btn btn-lg btn-primary w-100 fs-6">Signup
+                                            <span class="loading-text" style="display: none;">
+                                                <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+                                                Loading...
+                                            </span>
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
                 </div>
           </div>
        </div> 
@@ -46,4 +153,6 @@
     </div>
 
 </body>
+<script src="main/js/main.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </html>
