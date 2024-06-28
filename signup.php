c531e245921d029e3f01976a9621b3dcfbeddb1e
chanie55 <anya.sc30@gmail.com>
2024-06-27 22:17:51 -0700

    partial login signup

--- signup.php
+++ signup.php
@@ -18,23 +18,14 @@
 
        <div class="row border rounded-5 p-3 bg-white shadow box-area">
 
-    <!--------------------------- Left Box ----------------------------->
-
-       <div class="col-md-6 rounded-4 d-flex justify-content-center align-items-center flex-column left-box" style="background-image: url('img/heyho.jpg'); background-size:cover;">
-           <div class="featured-image mb-3">
-            <!-- <img src="img/heyho.jpg" class="img-fluid" style="width: 250px;"> -->
-           </div>
-           <p class="text-white fs-2" style="font-family: 'Courier New', Courier, monospace; font-weight: 600;">MPM Furniture</p>
-           <small class="text-white text-wrap text-center" style="width: 17rem;font-family: 'Courier New', Courier, monospace;">Find the best furniture in this platform.</small>
-       </div> 
 
     <!-------------------- ------ Right Box ---------------------------->
         
-       <div class="col-md-6 right-box">
+       <div class="col-md-12 right-box">
           <div class="row align-items-center">
                 <div class="header-text mb-4">
-                     <h2>Hello!</h2>
-                     <p>We are happy to have you back.</p>
+                     <h2>Register</h2>
+                     <p>Some text here</p>
                 </div>
                 <div class="input-group mb-3">
                     <input type="text" class="form-control form-control-lg bg-light fs-6" placeholder="Email address">
@@ -42,20 +33,11 @@
                 <div class="input-group mb-1">
                     <input type="password" class="form-control form-control-lg bg-light fs-6" placeholder="Password">
                 </div>
-                <div class="input-group mb-5 d-flex justify-content-between">
-                    <div class="form-check">
-                        <input type="checkbox" class="form-check-input" id="formCheck">
-                        <label for="formCheck" class="form-check-label text-secondary"><small>Remember Me</small></label>
-                    </div>
-                    <div class="forgot">
-                        <small><a href="#">Forgot Password?</a></small>
-                    </div>
-                </div>
                 <div class="input-group mb-3">
                     <button class="btn btn-lg btn-primary w-100 fs-6">Login</button>
                 </div>
                 <div class="row">
-                    <small>Don't have an account? <a href="signup.php">Sign Up</a></small>
+                    <small>Already have an account? <a href="login.php">Login</a></small>
                 </div>
           </div>
        </div> 
