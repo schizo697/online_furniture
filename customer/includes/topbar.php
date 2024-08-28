06456918092881739f6fdc565e0306c3b383ef1b
Ann <fernandez.226952@gensan.sti.edu.ph>
2024-08-28 11:18:58 +0800

    ayos topbar customer

--- customer/includes/topbar.php
+++ customer/includes/topbar.php
@@ -54,8 +54,9 @@ include '../conn.php';
         <div class="container topbar bg-primary d-none d-lg-block">
             <div class="d-flex justify-content-between">
                 <div class="top-info ps-2">
-                    <small class="me-3"><i class="fas fa-map-marker-alt me-2 text-secondary"></i> <a href="#" class="text-white">123 Street, New York</a></small>
-                    <small class="me-3"><i class="fas fa-envelope me-2 text-secondary"></i><a href="#" class="text-white">Email@Example.com</a></small>
+                    <small class="me-3"><i class="fas fa-map-marker-alt me-2 text-secondary"></i> <a href="#" class="text-white">Purok 1 Carinal Street Brgy. Ligaya General Santos City</a></small>
+                    <small class="me-3"><i class="fas fa-envelope me-2 text-secondary"></i><a href="#" class="text-white">marvin26manansala@gmail.com</a></small>
+                    <small class="me-3"><i class="fas fa-solid fa-phone me-2 text-secondary"></i><a href="#" class="text-white">+639977694808</a></small>
                 </div>
                 <img src="img/Logo1.png" alt="Logo" class="logo">
             </div>
